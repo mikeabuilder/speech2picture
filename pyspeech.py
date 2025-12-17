700c979de02b662c23fdc3ff27839a95771710cd
mike <mike4nospam@comcast.net>
2025-12-17 15:49:29 -0800

    QRinst 4 - trying to fix stack order

--- pyspeech.py
+++ pyspeech.py
@@ -891,9 +891,9 @@ def create_main_window(usingHardwareButton):
  
     labelTextLong.grid(   row=0, column=1, columnspan=4, padx=(0,0),            sticky=tk.EW)
     labelForImage.grid(   row=0, column=6, rowspan=5,    padx=(0,0),   pady=10, sticky=tk.NSEW)
-    
-    labelQRForImage.grid(             row=0, column=6, rowspan=5, padx = (0,0),   pady=10)#rowspan = 5
     labelInstructionForDownload.grid( row=0, column=6, rowspan=5, padx = (0,0),   pady=10)#rowspan = 5
+    labelQRForImage.grid(             row=0, column=6, rowspan=5, padx = (0,0),   pady=10)#rowspan = 5
+    
 
     labelQR.grid(         row=1, column=2,               padx=(0,10),  pady=10, sticky=tk.NSEW)
     labelQRText.grid(     row=1, column=3,               padx=(10,0),  pady=10, sticky=tk.W)
