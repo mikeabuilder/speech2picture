4cc9a2b27b1f53806b9cdf5e4da4bc0c35acfd82
mike <mike4nospam@comcast.net>
2025-12-17 16:05:33 -0800

    Qr inst 6 showing QR image

--- pyspeech.py
+++ pyspeech.py
@@ -1135,8 +1135,8 @@ def display_image(image_path, label=None, labelQR = None, label_inst = None):
 
             # conver to photoImage
             QR_photo = ImageTk.PhotoImage(QRimg)
-            #labelQR.configure(image = QR_photo)
-            #labelQR.image = QR_photo  # keep a reference to prevent garbage collection
+            labelQR.configure(image = QR_photo)
+            labelQR.image = QR_photo  # keep a reference to prevent garbage collection
 
             inst_img = Image.open("download_instructions.jpg")
             inst_img = inst_img.resize((QR_size, QR_size), Image.NEAREST)
